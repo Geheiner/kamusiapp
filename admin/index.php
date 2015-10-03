6dada6de3bf6f6e2f4fb7ed18774f0f751de1b23
Joey Zenhäusern <joey.zenhausern@epfl.ch>
2015-10-04 00:33:57 +0200

    Fix js name

--- admin/index.php
+++ admin/index.php
@@ -5,7 +5,7 @@
     </head>
     <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="js/server_requests"></script>
+    <script src="js/server_requests.js"></script>
     <h1>Admin Panel</h1>
     <ul>
         <li><a onclick="loadGameLanguages()">Manage Games & Languages</li>
