8023a56a1a6442a10ac1b1ed278649f4c427135a
Joey Zenhäusern <joey.zenhausern@epfl.ch>
2015-12-15 11:27:33 +0100

    remove old language list

--- index.php
+++ index.php
@@ -380,22 +380,6 @@ $partOfSpeechArray= array('noun' => _("noun"), 'verb' => _('verb'), 'adjective' 
     <br><br>
     <br><br>
     <select id="menuLanguage" onchange="" size="1">
-    <!-- Preserving the list to add language translations to the database
-        <option><?php echo ("English"); ?></option>
-        <option><?php echo ("Deutsch"); ?></option>
-        <option><?php echo ("Español"); ?></option>
-        <option><?php echo ("Esperanto"); ?></option>
-        <option><?php echo ("Français"); ?></option>
-        <option><?php echo ("Gaeilge"); ?></option>
-        <option><?php echo ("Italiano"); ?></option>
-        <option><?php echo ("Kirundi"); ?></option>
-        <option><?php echo ("Kiswahili"); ?></option>
-        <option><?php echo ("Nederlands"); ?></option>
-        <option><?php echo ("Русский язык"); ?></option>
-        <option><?php echo ("Việt"); ?></option>
-
-        The languages are not translated yet
-            <option><?php echo ("Swahili"); ?></option> -->
     </select>
     <br><br>
     <br><br>
